ed42a9cb97bf5f52ac1ff880765f098f28b1e23e
chilos <combatburrito@gmail.com>
2026-03-09 17:44:25 -0700

    key generation. todo: ls-2

--- app/src/main/java/org/example/App.java
+++ app/src/main/java/org/example/App.java
@@ -1,9 +1,12 @@
 package org.example;
 
+import java.util.Arrays;
 import java.util.HashMap;
 
 public class App {
     HashMap<String, byte[]> data = new HashMap<>();
+    private byte[] p10Key1 = new byte[8];
+    private byte[] p10Key2 = new byte[8];
 
     App() {
         RAWDATA();
@@ -20,8 +23,41 @@ public class App {
         return new byte[0];
     }
 
+    // P10(k1, k2, k3, k4, k5, k6, k7, k8, k9, k10) = (k3, k5, k2, k7, k4, k10, k1,
+    // k9, k8, k6
+    private void KeyGeneration(byte[] rawkey) {
+        byte[] p10 = data.get("P10").clone();
+        byte[] rawkeyClone = rawkey.clone();
+        for (int i = 0; i < p10.length; i++) {
+            rawkeyClone[i] = rawkey[p10[i] - 1]; // permutation
+        }
+        byte[] ls1 = new byte[5];
+        byte[] ls2 = new byte[5];
+        // ls-1
+        for (int i = 0; i < 5; i++) {
+            if (i - 1 < 0) {
+                ls1[i] = rawkeyClone[i + 5];
+            } else {
+                ls1[i] = rawkeyClone[i - 1];
+            }
+        }
+        // ls-1
+        for (int i = 5; i < 10; i++) {
+            if (i - 1 < 5) {
+                ls2[i] = rawkeyClone[i + 5];
+            } else {
+                ls2[i] = rawkeyClone[i - 1];
+            }
+        }
+        for (int i = 0; i < 5; i++) {
+            p10Key1[i] = ls1[i];
+        }
+        for (int i = 0; i < 5; i++) {
+            p10Key1[i + 5] = ls2[i + 5];
+        }
+    }
+
     void RAWDATA() {
-        // Raw Key Plaintext Ciphertext
         byte[] rawKey0 = { 0, 0, 0, 0, 0, 0, 0, 0, 0, 0 };
         byte[] rawKey1 = { 1, 1, 1, 0, 0, 0, 1, 1, 1, 0 };
         byte[] rawKey2 = { 1, 1, 1, 0, 0, 0, 1, 1, 1, 0 };
